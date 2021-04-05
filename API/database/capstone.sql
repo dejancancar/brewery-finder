90b2b63fc7099e4c26a270bd11648380194f303d
toddzucker <78130195+toddzucker@users.noreply.github.com>
2021-04-05 11:05:17 -0700

    added breweries, locations, and hours tables to our databse

--- API/database/capstone.sql
+++ API/database/capstone.sql
@@ -21,11 +21,48 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
 	user_role varchar(50) NOT NULL
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	CONSTRAINT PK_user_id PRIMARY KEY (user_id)
+)
+
+CREATE TABLE breweries (
+	brewery_id int IDENTITY(1,1) NOT NULL,
+	user_id int NOT NULL,
+	phone nvarchar(15) NOT NULL,
+	history nvarchar (2000) NOT NULL,
+	is_active bit NOT NULL,
+	CONSTRAINT PK_brewery_id PRIMARY KEY (brewery_id),
+	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+)
+
+CREATE TABLE locations (
+	location_id int IDENTITY(1,1) NOT NULL,
+	brewery_id int NOT NULL,
+	street_address nvarchar(400) NOT NULL,
+	city nvarchar(200) NOT NULL,
+	zip_code nvarchar (15) NOT NULL,
+	CONSTRAINT PK_location_id PRIMARY KEY (location_id),
+)
+
+CREATE TABLE hours (
+	hours_id int IDENTITY (1,1) NOT NULL,
+	location_id int NOT NULL,
+	day_of_week int NOT NULL,
+	open_time time NOT NULL,
+	close_time time NOT NULL,
+	CONSTRAINT PK_hours_id PRIMARY KEY (hours_id),
+	CONSTRAINT FK_locations_id FOREIGN KEY (location_id) REFERENCES locations(location_id),
 )
 
 --populate default data
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+--INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+--instead of just user we have 3 users for the 3 breweries
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user1','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user2','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user3','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
+
 GO
+
+--platform,  great lakes,  fat head
+
