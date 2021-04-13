f3adb8a1b907c3750b9cffabc74f539758caba83
Clayton Cross <crosscribbs@gmail.com>
2021-04-13 16:01:25 -0400

    Beers added

--- API/database/capstone.sql
+++ API/database/capstone.sql
@@ -86,6 +86,10 @@ CREATE TABLE beers (
 	is_active bit NOT NULL
 	CONSTRAINT PK_beer_id PRIMARY KEY (beer_id),
 	CONSTRAINT FK_beers_breweries FOREIGN KEY (brewery_id) REFERENCES breweries(brewery_id)
+--INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+--VALUES (1, 'Fleeb Juice', 'Fleeb Juice is a hazy, soft, and juicy IPA carefully
+--crafted to exhibit notes of bright papaya, stone fruit, and mango.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618328482/wm9iiblo2ml9esv65a1j.jpg',
+--'FLJ','IPA','1')
 
 
 )
@@ -132,6 +136,11 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Brick_And_
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Collision_Bend_Brewing_Company','Jg45HuwT7PZkfuKTz6IB90CtWY4=', 'LHxP4Xh7bN0=','brewer');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Hansa_Brewery','Jg45HuwT7PZkfuKTz6IB90CtWY4=', 'LHxP4Xh7bN0=','brewer');
 
+--Demo Great Lakes User Data
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Great_Lakes_Brewing_Company','Jg45HuwT7PZkfuKTz6IB90CtWY4=', 'LHxP4Xh7bN0=','brewer');
+
+
+
 
 --breweries data
 INSERT INTO breweries (user_id, brewery_name, history, street_address, phone, city, zip_code, is_active)
@@ -159,6 +168,13 @@ INSERT INTO breweries (user_id, brewery_name, history, street_address, phone, ci
 VALUES ('13','Collision Bend Brewing Company', 'Fashionable waterfront brewery & taproom for house-brewed beers & New American small plates.', '1250 Old River Rd', '216-273-7879', 'Cleveland', '44113', 'true');
 INSERT INTO breweries (user_id, brewery_name, history, street_address, phone, city, zip_code, is_active)
 VALUES ('14','Hansa Brewery', 'Brewery, biergarten & eatery offering German-style beer & European eats in modern digs with a patio.', ' 2717 Lorain Ave', '216-631-6585', 'Cleveland', '44113', 'true');
+
+--Demo Great Lakes Data
+INSERT INTO breweries (user_id, brewery_name, history, street_address, phone, city, zip_code, is_active)
+VALUES ('15','Great Lakes Brewing Company', 'Iconic microbrewery with house suds, an updated American menu, free tours & a patio.', '2516 Market Ave', '216-771-4404', 'Cleveland', '44113', 'true');
+
+
+
 
 --hours data
 INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
@@ -342,6 +358,23 @@ INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, 
 VALUES (12, '7', '0', '0', '0', '0', '0', '0','0');
 
 
+--Demo Great Lakes Hours
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '1', '0', '0', '0', '0', '0', '0','0');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '2', '0', '0', '0', '0', '0', '0','0');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '3', '0', '0', '0', '0', '0', '0','0');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '4', '4', '00', 'pm', '10', '00', 'pm','1');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '5', '12', '00', 'pm', '10', '00', 'pm','1');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '6', '12', '00', 'pm', '10', '00', 'pm','1');
+INSERT INTO hours (brewery_id, day_of_week, open_hour, open_minute, open_am_pm, close_hour, close_minute, close_am_pm, is_closed)
+VALUES (13, '7', '12', '00', 'pm', '6', '00', 'pm','1');
+
+
 --brewery images data
 INSERT INTO brewery_images (brewery_id, image_url)
 VALUES (1, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618175380/jphovma3jurugvtnhxul.jpg');
@@ -426,6 +459,205 @@ INSERT INTO brewery_images (brewery_id, image_url)
 VALUES (12, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618185921/kll3kbycfgbdrzpdlvl8.jpg');
 INSERT INTO brewery_images (brewery_id, image_url)
 VALUES (12, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618185983/hoj9erozy2ci1urt3mdh.jpg');
+
+--Demo Great Lakes Images
+INSERT INTO brewery_images (brewery_id, image_url)
+VALUES (13, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618327191/wdcyxawpocwhtsizbpzt.jpg');
+INSERT INTO brewery_images (brewery_id, image_url)
+VALUES (13, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618327187/esbjlqbstpwrpz27tfyh.jpg');
+INSERT INTO brewery_images (brewery_id, image_url)
+VALUES (13, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618327195/ak32dt3bffxdjwou4tkg.jpg');
+INSERT INTO brewery_images (brewery_id, image_url)
+VALUES (13, 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618327681/vyhkiyetuu9mbn4gnptd.jpg');
+
+
+--beer images data
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (1, 'Fleeb Juice', 'Fleeb Juice is a hazy, soft, and juicy IPA carefully
+crafted to exhibit notes of bright papaya, stone fruit, and mango.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618328482/wm9iiblo2ml9esv65a1j.jpg',
+'8','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (2, 'Nitro Coconut Truffle', 'This imperial milk stout has a slightly sweet,
+nutty aroma of coconut that balances rich and roasty notes of chocolate .', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618329533/zluphr7pvmx4569d22lg.jpg',
+'10','Stout','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (3, 'Kurbisleichen Pumpkin Ale', 'The perfect autumn beer! 
+A full bodied seasonal brew with pumpkin sweetness and aromatic notes of allspice, nutmeg, ginger and cloves', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618330056/h0woyunx0ptb9sxtdhvr.jpg',
+'8.2','Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (4, 'Ohio Is For Lovers', 'This beer was dry-hopped with Fresh Wet Cashmere and Nugget from Spearhead hop farms', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618330629/hpj2loma40sbgm1b3r2g.jpg',
+'6.8','Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (5, 'Dawn of Ostara', 'Hopped exclusively with the PB blend for big orange creamsicle and mango! ', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618331242/pguwytmevatjkf65xzvl.jpg',
+'6.4','Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (6, 'Galactic Whoopsie', 'Big malt flavors and depth, with a touch of bitterness and maple syrup fullness! ', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618331554/nomdovd6tob8klrnbjcz.jpg',
+'7.1','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (7, 'Frosty Tangerline Lime Wit', 'Aged in Tequila Barrels and brewed with spicy Belgian yeast that plays with the limes', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618331688/qbzwasrg7pwla1dfkzi3.png',
+'5.5','Wheat Beer','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (8, 'Juicy Asap', 'Tropical, citrusy, and most certainly juicy.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618332498/xpndjzhdomvt7albz013.webp',
+'6.5','Pale Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (9, 'Citivopapotomus ', 'Brewed with the same base malt bill of wheat and pilsner malt as its sibling Citropapotomus,
+but with a hippo sized dose of new bling bling Citiva hops.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618332880/rmb531pmwabovyx4ezzw.jpg',
+'6.6','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (10, 'Bitter Chief IPA', 'Nice bitterness with refreshing citrus and grassy flavors and aromas. Malty finish balancing out the bitterness.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618333114/e6h64ee1fa9cu7zuw0g5.jpg',
+'6.9','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (11, 'C-Town', ' American style beer brewed by Collision Bend Brewing Company in Cleveland', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618333549/chmf0oct2vbpzf7oxard.jpg',
+'6.7','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (12, 'J.U.G', 'Hefeweizen yeast lends a hint of banana and clove with soft wheat graininess.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618334049/amvthojxj50uyt8ijn6e.jpg',
+'4.5','Wheat Beer','1');
+
+
+--Demo Great Lakes Beers
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Dortmunder Gold Lager', 'A humble hometown hero, draped in a people-pleasing blend of smooth malt and crisp hop flavors.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618334881/cndbugjbxsaruazpgufb.jpg',
+'5.8','Lager','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Elliot Ness', 'Almost untouchably smooth--armed with lightly toasted malts and noble hops.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618334756/iokowwlcblgkxn7sd4dp.jpg',
+'6.1',' Amber Lager','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Burning River', 'Crisp and bright, with refreshing flickers of citrus and pine that ignite the senses (not our local waterways).', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618335049/dhqqfqyqtzqmhzeamn3r.jpg',
+'6','Pale Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Great Lakes IPA', 'Hike, bike, sail, or laze your way through a dry-hopped,
+easy-drinking American IPA that’s perfectly suited for wherever the trail takes you.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618335186/yido7cntnmeqxpwe8apn.jpg',
+'6.5','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Comodore Perry', 'Victoriously hopped, with an arsenal of caramel malt flavors befitting the “Hero of Lake Erie.”', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618335456/gu8aplalwajw8tnyjunn.jpg',
+'7.7','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Edmund Fitzgerald', 'Brewed in memory of the sunken freighter, with rich roasted barley and bittersweet chocolate-coffee notes.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618338865/ndt9dxhirvbm6ztpy9ui.jpg',
+'6','Porter','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Hazecraft', 'Blast into orbit through a hazy galaxy of otherworldly tropical fruit aromas', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618339328/ne7hcittpskpcrhqvcu7.jpg',
+'6.7','IPA','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Crushworthy', 'Do you believe in love at first sip? With a lightly filtered body and a sweet kiss of orange, it is more than a crush. It is your new main squeeze.
+', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618339515/bpthdifiddduty491pdt.jpg',
+'4','Citrus Wheat','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Conway Irish Ale', 'Stationed at the intersection of sweet and toasty, 
+where biscuit and caramel malt flavors always have the right-of-way.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618339869/xmpyx3enlmalg8exqbxs.jpg',
+'6.3','Ale','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Bierwolf', 'Claw through a feral fusion of cold brew, chocolate, banana, and spice', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618340126/myvsvpsme40lohlnllcp.jpg',
+'6.6','Hybrid','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Mexican Lager With Lime', 'Spread a blanket and luxuriate in sunny rays of zesty lime and crisp, clean malt flavors.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618340861/qeyu0vbyxj4uw6cgxzjl.png',
+'5.4','Lager','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Chillwave', 'Glide across waves of tropical Mosaic hops and sweet honey malt.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618341004/f7kw5civmnocjqrscnrf.jpg',
+'9','IPA','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Rally Drum Premium Lager', 'An all-star roster of malt and hops swings for the fences and slides into a crisp,
+clean finish in this classic American Lager.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618341130/npqmiqr8shlv05asarkx.jpg',
+'4.2','Lager','1');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Lemon Hefeweizen', 'Fresh and zestful as boardwalk lemonade, with a clean, effervescent body that travels light.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618341291/swfqzfmgj1t6lblkjmw0.jpg',
+'5.1','Ale','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Lake Erie Monster Imperial IPA', 'Face your fears with a full-bodied hop behemoth', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618341531/kerifuhyrai5id1tbujm.jpg',
+'9.5','IPA','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Oktoberfest', 'Über smooth with vibrant malt flavors and a festive flourish of noble hops (lederhosen not included).', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618341669/tx2gwbqdvnncfph0gqla.png',
+'6.5','Lager','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, '73 Kolsch', 'Reliably crisp and smooth, with consecutive snaps of earthy hop aromas and subtle rye bitterness', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618342631/ptupdqvo2xabdgt0r0pu.jpg',
+'5.7','Hybrid','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Tidal Fury Imperial Hazy IPA', 'A tempest of tropical fruit aromas rushes from the cloudy depths in this Imperial Hazy IPA,
+brewed with an epic blast of Trident™ hops fit for a king.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618342778/dvla6b0gzbguxqyvcjql.jpg',
+'8','IPA','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Nosferatu Imperial Red IPA', 'Sink your teeth into rich, roasted malt and a stunning hop bite.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618342962/cz07ph6quzgxre1cy2pa.jpg',
+'8','IPA','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Ohio City Oatmeal Stout', 'Oh, what fun it is to ride through a flurry of flaked oats o’er a field of rich roasted malt flavors!', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618343113/fdf1gxhy9ydmjmhv8gzz.jpg',
+'5.4','Stout','0');
+
+INSERT INTO beers (brewery_id, beer_name, description, image_url, abv, beer_type, is_active)
+VALUES (13, 'Christmas Ale', 'A holiday classic, fully decked with fresh ginger, cinnamon, and sweet honey.', 'https://res.cloudinary.com/breweryfinderte/image/upload/v1618343254/odxp9bxh66serr4c1ns1.png',
+'7.5','Ale','0');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
